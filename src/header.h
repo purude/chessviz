2898208d3d0165712906466da13f94c1a0b254b3
Anton <purudeoff@gmail.com>
2020-05-20 09:56:58 +0700

    Add upgraded funcs in header file

--- src/header.h
+++ src/header.h
@@ -6,6 +6,7 @@ using namespace std;
 
 void Chess(char pole[][9]);
 void OprKoordinat(string xod, int k[2][2]);
-void MovePeshki(char pole[][9], int k[2][2]);
+void MoveAll(char pole[][9], int k[2][2]);
+int MoveCheck(int team, int k[][2], char pole[][9]);
 bool friendly(int side, int k[][2], char pole[][9]);
 #endif
