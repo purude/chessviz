51f94e5d6840e31e85d5535d50d7e72ca8b95f58
Anton <purudeoff@gmail.com>
2020-05-20 09:06:34 +0700

    Add new func in header file

--- src/header.h
+++ src/header.h
@@ -7,5 +7,5 @@ using namespace std;
 void Chess(char pole[][9]);
 void OprKoordinat(string xod, int k[2][2]);
 void MovePeshki(char pole[][9], int k[2][2]);
-
+bool friendly(int side, int k[][2], char pole[][9]);
 #endif
