47c35c37f5453eb4d598bae8ceb6aef856dbf8fb
Anton <purudeoff@gmail.com>
2020-05-21 20:13:30 +0700

    Add main func in main.cpp file for test and libs

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include "../src/source.h"
+#include "gtest/gtest.h"
+#include <string>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
