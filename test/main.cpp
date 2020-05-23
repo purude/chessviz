0445d9890cb7645d0f77477e95ff9a7fa18b3e83
Anton <purudeoff@gmail.com>
2020-05-23 18:30:47 +0700

    Create 2 more tests

--- test/main.cpp
+++ test/main.cpp
@@ -20,7 +20,7 @@ TEST(Down_peshka, Correct_p_forward1cell)
     array[0][0] = 5;
     array[1][1] = 2;
     array[1][0] = 5;
-    int result = MoveCheck(0, array, pole) int expected = 1;
+    int result = MoveCheck(1, array, pole) int expected = 1;
     EXPECT_EQ(expected, result);
 }
 TEST(Down_peshka, Correct_p_forward2cell)
@@ -30,7 +30,7 @@ TEST(Down_peshka, Correct_p_forward2cell)
     array[0][0] = 5;
     array[1][1] = 3;
     array[1][0] = 5;
-    int result = MoveCheck(0, array, pole);
+    int result = MoveCheck(1, array, pole);
     int expected = 1;
     EXPECT_EQ(expected, result);
 }
@@ -41,7 +41,7 @@ TEST(Down_peshka, Correct_p_attack)
     array[0][0] = 5;
     array[1][1] = 2;
     array[1][0] = 4;
-    int result = MoveCheck(0, array, pole);
+    int result = MoveCheck(1, array, pole);
     int expected = 1;
     pole[2][4] = 'R';
     EXPECT_EQ(expected, result);
@@ -82,6 +82,34 @@ TEST(Up_peshka, Correct_P_attack)
     pole[5][5] = ' ';
     EXPECT_EQ(expected, result);
 }
+
+TEST(Up_Rook, Correct_R_left)
+{
+    int array[2][2];
+    array[0][1] = 7;
+    array[0][0] = 1;
+    array[1][1] = 5;
+    array[1][0] = 1;
+    pole[6][1] = ' ';
+    int result = MoveCheck(0, array, pole);
+    int expected = 1;
+    pole[6][1] = 'P';
+    EXPECT_EQ(expected, result);
+}
+TEST(Up_Rook, Correct_R_right)
+{
+    int array[2][2];
+    array[0][1] = 7;
+    array[0][0] = 8;
+    array[1][1] = 5;
+    array[1][0] = 8;
+    pole[6][8] = ' ';
+    int result = move_check(0, array, pole);
+    int expected = 1;
+    pole[6][8] = 'P';
+    EXPECT_EQ(expected, result);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
